f65577d6a897b911e5b353e7c9bf6a871ed74c2d
Safder Iqbal <safder.iqbal@gmail.com>
2014-10-25 23:26:07 +0100

    Beginning API integration

---
+++ DrunkCheckAPI/APIs/ClockWorkSMS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrunkCheck.APIs
+{
+    public class ClockWorkSMS
+    {
+        private static readonly string ApiKey = ConfigurationManager.AppSettings.Get("ClockWorkSMSAPIKey");
+    }
+}
