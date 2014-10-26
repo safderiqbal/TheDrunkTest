6a721fabead61f994c0b03e1bd59a4f384c34645
Safder Iqbal <safder.iqbal@gmail.com>
2014-10-26 03:36:10 +0000

    Final clockwork stuuf?

--- DrunkCheckAPI/DrunkCheck/Controllers/ClockWorkController.cs
+++ DrunkCheckAPI/DrunkCheck/Controllers/ClockWorkController.cs
@@ -1,14 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using DrunkCheck.APIs;
 
 namespace DrunkCheck.Controllers
 {
     public class ClockWorkController : Controller
     {
-        public JsonResult ClockworkTest()
+        public JsonResult SendSms(int recipientNumber, string message)
         {
-            bool success = ClockWorkSms.SendTestMessage();
+            bool success = ClockWorkSms.SendMessage(recipientNumber, message);
             return Json(new {success}, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ReceiveSms(string to, string from, string content, string keyword)
+        {
+            ClockWorkSms.SendMessage(Convert.ToInt32(from), content);
+            return Json(true, JsonRequestBehavior.AllowGet);
         }
     }
 }
