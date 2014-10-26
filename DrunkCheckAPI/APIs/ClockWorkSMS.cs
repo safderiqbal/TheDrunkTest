60ee81bca71c634a4dc4eb927069e65116737141
Safder Iqbal <safder.iqbal@gmail.com>
2014-10-26 01:46:43 +0000

    More clockwork setup

--- DrunkCheckAPI/APIs/ClockWorkSMS.cs
+++ DrunkCheckAPI/APIs/ClockWorkSMS.cs
@@ -1,14 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using Clockwork;
 
 namespace DrunkCheck.APIs
 {
-    public class ClockWorkSMS
+    public static class ClockWorkSms
     {
         private static readonly string ApiKey = ConfigurationManager.AppSettings.Get("ClockWorkSMSAPIKey");
+
+        public static bool SendTestMessage()
+        {
+            API api = new API(ApiKey);
+            try
+            {
+                SMSResult result = api.Send(new SMS {To = "441234567890", Message = "Hello World"});
+
+                return result.Success;
+            }
+            catch (APIException ex)
+            {
+                // You'll get an API exception for errors
+                // such as wrong username or password
+                Console.WriteLine("API Exception: " + ex.Message);
+            }
+            catch (WebException ex)
+            {
+                // Web exceptions mean you couldn't reach the Clockwork server
+                Console.WriteLine("Web Exception: " + ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                // Argument exceptions are thrown for missing parameters,
+                // such as forgetting to set the username
+                Console.WriteLine("Argument Exception: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Something else went wrong, the error message should help
+                Console.WriteLine("Unknown Exception: " + ex.Message);
+            }
+            return false;
+        }
     }
 }
---
+++ DrunkCheckAPI/DrunkCheck/Controllers/ClockWorkController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using DrunkCheck.APIs;
+
+namespace DrunkCheck.Controllers
+{
+    public class ClockWorkController : Controller
+    {
+        public JsonResult ClockworkTest()
+        {
+            bool success = ClockWorkSms.SendTestMessage();
+            return Json("{success: " + success.ToString().ToLower() + "}", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
