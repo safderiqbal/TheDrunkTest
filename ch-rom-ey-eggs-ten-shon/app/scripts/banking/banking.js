7ad0c951a82111f86c8b79eecad8e805cb0c3447
Si Levy <simonjlevy@gmail.com>
2014-10-26 03:43:45 +0000

    all of your options are open to me. and some other crap.

--- ch-rom-ey-eggs-ten-shon/app/scripts/banking/banking.js
+++ ch-rom-ey-eggs-ten-shon/app/scripts/banking/banking.js
@@ -4,37 +4,50 @@ angular.module('thedrunktest', []).directive('overlay', function () {
   };
 });
 
-// mask until the call to the service returns.
-$('body').append('<div id="temp" style="width:100%; height:100%; background:white; opacity: 0.7; position: absolute; z-index:9998; top: 0px; left:0px;"></div>');
-$('body').append('<div id="temp2" style="font-size: 20em; text-align: center; color: black; position: absolute; z-index:9999; top: 0px; left:0px;">Please Blow</div>');
+var $http = angular.injector(['ng']).get('$http');
 
-$.ajax('http://drunkchecker.azurewebsites.net/').done(function (result) {
-  // setup the angular app
-  $('body').attr('ng-app', 'thedrunktest');
+chrome.storage.sync.get('id', function (value) {
+  console.log(value.id);
+  if (value.id) {
+    // mask until the call to the service returns.
+    $('body').append('<div id="temp" style="width:100%; height:100%; background:white; opacity: 0.7; position: absolute; z-index:9998; top: 0px; left:0px;"></div>');
+    $('body').append('<div id="temp2" style="font-size: 20em; text-align: center; color: black; position: absolute; z-index:9999; top: 0px; left:0px;">Please Blow</div>');
+    $http.get('http://drunkchecker.azurewebsites.net/ReadForUser', {
+      params: {
+        id: value.id
+      }
+    }).then(function (result) {
+      console.log(result);
+      // setup the angular app
+      $('body').attr('ng-app', 'thedrunktest');
 
-  if (result.success && result.value > 100) {
-    // you're too drunk -> show overlay.
-    console.log("you pisshead! reading: " + result.value);
+      if (result.data.success && result.data.value > 100) {
+        // you're too drunk -> show overlay.
+        console.log("you pisshead! reading: " + result.data.value);
 
-    // remove everything on page
-    $('body').children().remove();
-    $('link').remove();
+        // remove everything on page
+        $('body').children().remove();
+        $('link').remove();
 
-    var $compile = angular.injector(['ng']).get('$compile'),
-      $rootScope = angular.injector(['ng']).get('$rootScope'),
-      overlayElement = $compile('<div overlay=""></div>')($rootScope.$new());
+        var $compile = angular.injector(['ng']).get('$compile'),
+          $rootScope = angular.injector(['ng']).get('$rootScope'),
+          overlayElement = $compile('<div overlay=""></div>')($rootScope.$new());
 
-    // add our custom splash overlay
-    $('body').append(overlayElement);
-  } else {
-    // you're sober, congrats. tell them.
-    console.log("you're sober.. booo! congrats I guess.");
-    $('#temp').remove();
-
+        // add our custom splash overlay
+        $('body').append(overlayElement);
+      } else {
+        // you're sober, congrats. tell them.
+        console.log("you're sober.. booo! congrats I guess.");
+        console.log("reading was: " + result.data.value);
+        $('#temp').remove();
+        $('#temp2').remove();
+      }
+    }, function () {
+      console.log('something went wrong whilst requesting a reading...');
+      console.log(arguments);
+      $('#temp').remove();
+      $('#temp2').remove();
+    });
   }
-}).fail(function () {
-  console.log('something went wrong whilst requesting a reading...');
-  console.log(arguments);
-  $('#temp').remove();
 });
 
--- ch-rom-ey-eggs-ten-shon/app/scripts/options.js
+++ ch-rom-ey-eggs-ten-shon/app/scripts/options.js
@@ -1,3 +1,74 @@
 'use strict';
+var store = chrome.storage.sync;
 
-console.log('\'Allo \'Allo! Option');
+store.get('email', function (value) {
+  console.log(value.email);
+});
+
+angular.module('app', []).controller('options', ['$scope', '$http', '$q', function($scope, $http, $q) {
+  store.get(['name', 'email', 'id'], function (value) {
+    $scope.email = value.email || '';
+    $scope.name = value.name || '';
+    $scope.id = value.id || '';
+    $scope.$evalAsync();
+  });
+
+  $scope.email = '';
+  $scope.name = '';
+  $scope.save =  function(e) {
+    $http.get('http://drunkchecker.azurewebsites.net/User/GetUser', {
+      params: { email: $scope.email }
+    }).then(function (result) {
+      if (result.data === '{success : false}') {
+        // email not registered. go register it
+        return $q.reject();
+      } else {
+        // email registered... update name if different
+        $scope.name = result.data.Name;
+        $scope.id = result.data.Id;
+        $scope.$evalAsync();
+
+        store.set({'id': result.data.Id}, function() {
+          console.log("saved Id: " + result.data.Id);
+        });
+      }
+    }, function () {
+      console.log(arguments);
+    }).then(undefined, function () {
+      $http.put('http://drunkchecker.azurewebsites.net/User/CreateUser', {
+        params: {
+          name: $scope.name,
+          email: $scope.email
+        }
+      }).then(function(result) {
+        store.set({'id': result.data.Id}, function() {
+          console.log("saved Id: " + result.data.Id);
+        });
+        $scope.id = result.data.Id;
+        $scope.$evalAsync();
+        console.log(arguments);
+      }, function() {
+        console.log(arguments);
+      })
+    });
+  };
+
+  $scope.$watch('email', function(newValue, oldValue) {
+    if (newValue !== oldValue) {
+      store.set({'email': newValue}, function() {
+        console.log("saved email: " + newValue);
+      });
+    }
+  });
+
+  $scope.$watch('name', function(newValue, oldValue) {
+    if (newValue !== oldValue) {
+      store.set({'name': newValue}, function() {
+        console.log("saved name: " + newValue);
+      });
+    }
+  });
+}]);
+
+
+
