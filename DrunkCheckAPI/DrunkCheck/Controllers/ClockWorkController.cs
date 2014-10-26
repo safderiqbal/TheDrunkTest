90b273478868bc6bc169c70bc33ef92082342647
Ben Lewis <ben.lewis@webappuk.com>
2014-10-26 05:42:13 +0000

    Hello guise

--- DrunkCheckAPI/DrunkCheck/Controllers/ClockWorkController.cs
+++ DrunkCheckAPI/DrunkCheck/Controllers/ClockWorkController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Web.Mvc;
 using DrunkCheck.APIs;
+using DrunkCheck.Models;
+using DrunkCheckUser = DrunkCheck.Models.User;
 
 namespace DrunkCheck.Controllers
 {
@@ -14,7 +16,39 @@ namespace DrunkCheck.Controllers
 
         public JsonResult ReceiveSms(string to, string from, string content, string keyword)
         {
-            ClockWorkSms.SendMessage(from, content);
+            string email = content.Substring(11);
+            User user = null;
+            if (email.Trim() != "")
+            {
+                user = DrunkCheckUser.Get(-1, email);
+            }
+
+            if (user == null)
+            {
+                ClockWorkSms.SendMessage(from, "Sorry but the that user has not been found. Blame Jeff");
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+
+            Reading reading;
+
+            DrunkCheckResponse response = DrunkCheckInterface.Read(user);
+            using (DrunkCheckerContext db = new DrunkCheckerContext())
+            {
+                reading = new Reading
+                {
+                    UserId = user.Id,
+                    DateTime = DateTime.Now,
+                    Value = response.value
+                };
+
+                db.Readings.Add(reading);
+                db.SaveChanges();
+            }
+
+            ClockWorkSms.SendMessage(from,
+                "You read a value of " + reading.Value + 
+                ", which must mean you are Drunk Level '" + response.drunkLevel);
+
             return Json(true, JsonRequestBehavior.AllowGet);
         }
     }
